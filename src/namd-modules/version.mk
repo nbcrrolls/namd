56f05fb62e5a7ad15973b63f65d078fd00067f82
nadyawilliams <nadya@sdsc.edu>
2015-01-16 11:06:46 -0800

    initial, switch to using modules

---
+++ src/namd-modules/version.mk
@@ -0,0 +1,33 @@
+ifndef ROLLCOMPILER
+  ROLLCOMPILER = gnu
+endif
+COMPILERNAME := $(firstword $(subst /, ,$(ROLLCOMPILER)))
+
+ifndef ROLLNETWORK
+  ROLLNETWORK = eth
+endif
+
+ifndef ROLLMPI
+  ROLLMPI = openmpi
+endif
+
+# define dependent modules that will need to be loaded  with namd module
+ADDCOMP = 
+ifeq ("$(ROLLCOMPILER)", "intel")
+  ADDCOMP = intel mkl
+endif
+PREREQMODULES = "$(ROLLMPI)-$(COMPILERNAME)-$(ROLLNETWORK) $(CUDA) $(ADDCOMP)"
+
+VERSION.MK.MASTER = version.mk
+VERSION.MK.MASTER.DIR = ../namd
+VERSION.MK.INCLUDE = namd.version.mk
+include $(VERSION.MK.INCLUDE) 
+
+PACKAGE     = namd
+CATEGORY    = applications
+NAME        = namd-module-$(COMPILERNAME)-$(ROLLNETWORK)
+RELEASE     = 0
+PKGROOT     = /opt/modulefiles/$(CATEGORY)/$(PACKAGE)$(VERSION)
+
+RPM.REQUIRES    = environment-modules
+
