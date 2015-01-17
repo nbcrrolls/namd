9b21e9d21af5ad9fc05b1b089b86430e2aa44a2c
nadyawilliams <nadya@sdsc.edu>
2015-01-16 18:10:47 -0800

    update to tcl 8.6.3. Switch to using NBCR.mk

---
+++ src/tcl/version.mk
@@ -0,0 +1,20 @@
+ifndef ROLLCOMPILER
+  ROLLCOMPILER = gnu
+endif
+COMPILERNAME := $(firstword $(subst /, ,$(ROLLCOMPILER)))
+
+VERSION.MK.MASTER = version.mk
+VERSION.MK.MASTER.DIR = ..
+VERSION.MK.INCLUDE = namd.version.mk
+include $(VERSION.MK.INCLUDE)
+
+NAME           = namd-tcl-$(COMPILERNAME)
+VERSION        = $(TCLVER)
+
+SOURCE_NAME    = tcl
+SOURCE_SUFFIX  = tar.gz
+SOURCE_VERSION = $(TCLVER)
+SOURCE_PKG     = $(SOURCE_NAME)$(SOURCE_VERSION)-src.$(SOURCE_SUFFIX)
+SOURCE_DIR     = $(SOURCE_PKG:%-src.$(SOURCE_SUFFIX)=%)
+
+TAR_GZ_PKGS    = $(SOURCE_PKG)
