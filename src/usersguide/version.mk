3849682a39da4ac25d86fe6480a7674fe68ce54b
nadyawilliams <nadya@sdsc.edu>
2015-01-20 18:31:51 -0800

    add arch

--- src/usersguide/version.mk
+++ src/usersguide/version.mk
@@ -4,12 +4,14 @@ VERSION.MK.MASTER.DIR = ..
 VERSION.MK.INCLUDE = namd.version.mk
 include $(VERSION.MK.INCLUDE)
 
+RPM.ARCH = noarch
+
 ROLL			= namd
 NAME    		= roll-$(ROLL)-usersguide
 
 SUMMARY_COMPATIBLE	= $(VERSION)
 SUMMARY_MAINTAINER	= Rocks Group
-SUMMARY_ARCHITECTURE	= x86_64
+SUMMARY_ARCHITECTURE	= i386, x86_64
 
 ROLL_REQUIRES		= base kernel os mpi
 ROLL_CONFLICTS		= 
