e9d6b0cbd95813a4949b4f40a0384be2e17a163e
nadyawilliams <nadya@sdsc.edu>
2015-06-08 19:48:13 -0700

    use same naming schema for all rpms

--- src/namd-opal/version.mk
+++ src/namd-opal/version.mk
@@ -11,11 +11,25 @@ ifndef ROLLMPI
   ROLLMPI = openmpi
 endif
 
+ifndef ROLLCUDA
+  ROLLCUDA = nocuda
+endif
+
+# check if need to add cuda module
+ifeq ("$(ROLLCUDA)", "cuda")
+  CUDA = -cuda
+  ADDCUDA = cuda
+  CUDAEXT = _cuda
+else
+  ADDCUDA =
+  CUDAEXT = 
+endif
+
 VERSION.MK.MASTER = version.mk
 VERSION.MK.MASTER.DIR = ..
 VERSION.MK.INCLUDE = namd.version.mk
 include $(VERSION.MK.INCLUDE)
 
-NAME = namd-opal
-VERSION        = 1.0
+NAME    = namd-opal-$(COMPILERNAME)-$(ROLLNETWORK)-$(ROLLMPI)-$(ROLLCUDA)
+VERSION = 1.0
 
