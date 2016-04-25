fdec33d1f953501f803cc5f5111e30bed97b70a8
nadyawilliams <nadya@sdsc.edu>
2016-04-25 15:52:27 -0700

    move sources to gogledrive

--- bootstrap.sh
+++ bootstrap.sh
@@ -11,6 +11,23 @@ if [ $# -ne 1 ]; then
     exit 0
 fi 
 
+if [ ! -d $NBCRDEVEL ]; then
+    echo "Need to install roll 'nbcr' first"
+    exit 1
+fi
+
+# set google url for dowloading sources
+. $NBCRDEVEL/bootstrap-values.sh
+
+# download sources
+. $ROLLSROOT/etc/bootstrap-functions.sh
+echo ""
+echo "WARNING: before making a roll download NAMD source from nbcr.ucsd@gmail.com goodledrive in privaterools/"
+echo ""
+
+
+exit
+
 # build and install fftw 
 (cd src/fftw; make ROLLCOMPILER=$1 rpm; make ROLLCOMPILER=$1 clean)
 rpm -i RPMS/x86_64/namd-fftw-$1*.rpm
